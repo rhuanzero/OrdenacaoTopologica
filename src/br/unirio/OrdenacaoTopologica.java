15b7e9c7a31788ea9f789d7a1d6e4846c9454ec5
Raul Rodrigues <91432196+RaulRJ5@users.noreply.github.com>
2025-07-06 11:45:37 -0300

    Terminando o gerarGrafo finalizando o ehAciclico

--- src/br/unirio/OrdenacaoTopologica.java
+++ src/br/unirio/OrdenacaoTopologica.java
@@ -305,16 +305,23 @@ public class OrdenacaoTopologica
 	//Gerador de numeros aleatorios
 	Random nAleatorio  = new Random();
 
+	//Vertice ainda nao visitado
+	private static final int NAO_VISITADO = 0;
+	//Vertice esta sendo visitado
+	private static final int VISITANDO = 1;
+	//Vertice ja foi visitado
+	private static final int VISITADO = 2;
+
 	//Metodo para gerar um grafo direcionado aciclico de N vertices
 	private void gerarGrafo(int nVertices){
-
-		//Definindo o numero de arestas do grafo como nVertices + 2
-		final int N_ARESTAS = nVertices + 2;
 
 		//Checando se o numero de vertices é igual ou menor que zero
 		if(nVertices <= 0) {
 			return;
 		}
+
+		//Definindo o numero de arestas do grafo como nVertices + 2
+		final int N_ARESTAS = nVertices + 2;
 
 		//Adicionando no arquivo.txt cada linha de adicao no grafo
 		//Utilizando o BufferedWriter pois tera casos com milhares de vertices
@@ -346,7 +353,7 @@ public class OrdenacaoTopologica
 				}
 
 				//Se a aresta faz permanencer o grafo aciclico, adiciona no arquivo
-				if(ehAciclico(x,y)) {
+				if(ehAciclico(x,y,aresta[x][y], nVertices)) {
 
 					//Adicionando aresta
 					aresta[x][y] = true;
@@ -367,8 +374,62 @@ public class OrdenacaoTopologica
 	}
 	
 	//Metodo para conferir se a insercao x < y ira manter o grafo aciclico 
-	private boolean ehAciclico(int x, int y) {
-		
+	private boolean ehAciclico(int x, int y, boolean[][] aresta, int nVertices) {
+
+		//Recebe o estado atual de aresta e o salva
+		boolean estadoOriginal = aresta[x][y];
+
+		//De forma temporaria iremos ter essa aresta como true
+		aresta[x][y] = true;
+
+		//Checa se coma aresta temporiamente adicionada aagora faz um ciclo ter um grafo
+		boolean temCiclo = contemCiclo(aresta[x][y], x, y, nVertices);
+
+		//Restaura o estado original da aresta
+		aresta[x][y] = estadoOriginal;
+
+		//Caso nao tiver ciclo, ou seja, !temCiclo, continuaa aciclico
+		//Caso temCiclo, logo e ciclico
+		return !temCiclo;
+	}
+
+	//Metodo para verificar se com a aresta adicionada temporaria ira fazer o grafo ser ciclico
+	private boolean contemCiclo(boolean[][] aresta, int x, int y, int nVertices) {
+		//Vetor para mapear os estado de cada vertice
+		int[] estados = new int[nVertices+1];
+
+		//Garantindo que todos vertices sejam verificados
+		for(int i = 1; i <= nVertices; i++) {
+			// Verificar se o vertice ainda nao foi visitado
+			if(estados[i] == NAO_VISITADO) {
+				if(buscaNoGrafo(aresta[x][y],x,y, nVertices, i, estados)){
+					return true; //Ciclo encontrado
+				}
+			}
+		}
+		return false; // Nao foi encontrado nenhum ciclo em todo grafo durante as recursoes
+	}
+
+	//Metodo recursivo para buscar caso o vertice ja foi visitado ou nao
+	private boolean buscaNoGrafo(boolean[][] aresta, int x, int y, int nVertices, int u,  int[] estados){
+		//Indice atual como visitando, estando na recursao
+		estados[u] = VISITANDO;
+
+		for(int v = 1; v <= nVertices; i++) {
+			if(aresta[x][y]) { //Se existe uma aresta x < y
+				// y ainda nao foi visitado
+				if(estados[v] == NAO_VISITADO) {
+					if(buscaNoGrafo(aresta[x][y], nVertices, v, estados)){
+						return true; // Se um ciclo for encontrado
+					}
+				}
+				else if(estados[v] == VISITADO) {
+					return true;// tem ciclo, x < y, forma um ciclo
+				}
+			}
+		}
+		estados[x] = VISITADO;
+		return false; // Nao foi encontrado nenhum ciclo
 	}
 
 
